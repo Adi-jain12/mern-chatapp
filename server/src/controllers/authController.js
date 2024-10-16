02381d9e50681c8bf0b5b5a5454dc4d03c817582
Adi-jain12 <jaditya98@gmail.com>
2024-10-16 13:13:43 +0530

    fix bug

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+import User from '../models/users.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -12,6 +12,7 @@ export const io = new Server(server, {
 	cors: {
 		origin: process.env.FRONTEND_URL,
 		methods: ['GET', 'POST'],
+		credentials: true,
 	},
 });
 
